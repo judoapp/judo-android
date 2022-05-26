58888b0bff26cae27ad0b1ed821fc52c38b43752
Andrew Clunis <andrew@orospakr.ca>
2022-05-25 23:34:46 -0400

    Releasing 1.8.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val judoVersion by extra("1.8.1-beta1")
+    val judoVersion by extra("1.8.1")
     val composeVersion by extra("1.0.1")
 
     repositories {
